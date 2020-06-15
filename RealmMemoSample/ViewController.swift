7ea539bf9226dbbabdd34343bc3c6207b228a5a5
Tomotaka-Shimizu <tomotaka1211@gmail.com>
2020-06-16 00:28:30 +0900

    Initial Commit

---
+++ RealmMemoSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmMemoSample
+//
+//  Created by 清水智貴 on 2020/06/16.
+//  Copyright © 2020 tomotaka.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
